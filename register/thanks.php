52eea72e487181d9de97fd2de2afa7eeac483e70
Yasuteru Ono <teruteruteru1@gmail.com>
2018-03-04 11:31:23 +0800

    サインアップ周り実装完了

--- register/thanks.php
+++ register/thanks.php
@@ -1,60 +1,3 @@
-<?php
-    session_start();
-
-    require('../dbconnect.php');
-
-    if (!isset($_SESSION['register'])) {
-        header('Location: signup.php');
-        exit();
-    }
-
-  //$_postは生きてるか？
-    echo '<pre>';
-    echo '$_POST= ';
-    var_dump($_POST);
-    echo '</pre>';
-
-    echo '<pre>';
-    echo '$_SESSION= ';
-    var_dump($_SESSION);
-    echo '</pre>';
-
-    $name = $_SESSION['register']['name'];
-    $email = $_SESSION['register']['email'];
-    $password = $_SESSION['register']['password'];
-    $img_name = $_SESSION['register']['img_name'];
-
-
-
-    // $_POSTがからじゃなければ処理＝ユーザー登録ボタンが押されたら
-    if (!empty($_POST)){
-        // パスワードのハッシュ化
-        $hash_password = password_hash($password, PASSWORD_DEFAULT);
-        echo $hash_password;
-        echo '<br>';
-
-        // STEP2
-        $sql = 'INSERT INTO `users` SET`user_name`=?,`email`=?,`password`=?,`img_name`=?,`created`=NOW()';
-        // ?の数と配列の数を合わせる
-        // プリペアードステイトメント
-        $data = array($name, $email, $hash_password, $img_name);
-        $stmt = $dbh->prepare($sql);
-        $stmt->execute($data);
-
-        // セッションのregisterを削除する(削除)
-        $_SESSION['register'] = array();
-        unset($_SESSION['register']);
-
-        header('Location: thanks.php');
-        exit();
-    }
-    // STEP3
-    $dbh = null;
-?>
-
-
-
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -68,7 +11,6 @@
   <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="../assets/css/animate.css">
   <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
-  <!-- <link rel="stylesheet" type="text/css" href="../assets/css/style.css"> -->
   <link rel="stylesheet" type="text/css" href="../assets/css/responsive.css">
 
   <link rel="stylesheet" type="text/css" href="../assets/css/signup.css">
@@ -91,7 +33,6 @@
             
             <div class="navbar-collapse collapse">
               <ul class="nav navbar-nav navbar-right">
-                <li><a href="#signin">ログイン</a></li>
                 <li><a href="signup.php">ユーザー登録</a></li>
                 <li><a href="#signout">ログアウト</a></li>
                 <li><a href="../home.php">マイページ</a></li>
@@ -108,36 +49,28 @@
 
 <div class="row">
     <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
-      <h2>確認画面 </h2>
+      <form method="POST" action="signup.php" enctype="multipart/form-data" role="form">
+
+      <h2>登録完了 </h2>
       <hr class="colorgraph">
       <div class="form-group">
-        <!-- <input type="text" name="input_name" id="name" class="form-control input-lg" tabindex="3"> -->
-        <p style="font-size: 20px">ユーザー名: <?php echo htmlspecialchars($name); ?></p><br>
-      <img src="../user_profile_img/<?php echo htmlspecialchars($img_name); ?>" width="100"><br>
+        <h2>登録ありがとうございます。</h2><br>
+        <h2>以下よりログインください。</h2>
+
 
       </div>
       <div class="form-group">
-        <!-- <input type="email" name="input_email" id="email" class="form-control input-lg" placeholder="メールアドレス
-        " tabindex="4"> -->
-        <p style="font-size: 20px">ユーザー名: email: <?php echo htmlspecialchars($email); ?></p><br>
+        
       </div>
       <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-6">
-          <div class="form-group">
-            <!-- <input type="password" name="input_password" id="password" class="form-control input-lg" placeholder="Password" tabindex="5"> -->
-            <p style="font-size: 20px">パスワード: ●●●●●●●●</p><br>
-
-          </div>
-        </div>
         <div class="col-xs-12 col-sm-6 col-md-6"></div>
       </div>
 
       <hr class="colorgraph">
       <div class="row">
         <form method="POST" action="check.php">
-          <input type="hidden" name="hoge" value="signal"> // value="signal"をPOST送信でcheck.phpに送信することにより
-          <div class="col-xs-12 col-md-6"><a href="signup.php?action=rewrite" class="btn btn-success btn-block btn-lg">戻る</a></div>
-          <div class="col-xs-12 col-md-6"><input type="submit" value="ユーザー登録" class="btn btn-lg btn-block btn-primary" tabindex="7"></div>
+          <input type="hidden" name="hoge" value="huga">
+          <div class="col-xs-12 col-md-6"><a href="../signin.php" class="btn btn-success btn-block btn-lg">ログイン</a></div>
         </form>
         
       </div>
