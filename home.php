9a4104b9584c9bffd62a711c0754b032e5d4f590
手代木悠也 <teshirogiyuya@teshirogiyuunarinoMacBook-puro.local>
2018-03-23 17:03:54 +0800

    masteに追いついた

--- home.php
+++ home.php
@@ -47,7 +47,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title></title>
+    <title>home</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
